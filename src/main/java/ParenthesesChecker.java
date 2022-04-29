d458b4b3388bcddd7b2dafae8c43f246ed18be0d
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-04-29 14:01:49 +0100

    first commit

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,12 @@
+public class ParenthesesChecker {
+    public ParenthesesChecker(){
+
+    }
+
+    public boolean checkParentheses(String input){
+        // check if input matches required string
+        if(input.equals("")){
+            return true;
+        } return false;
+    }
+}
